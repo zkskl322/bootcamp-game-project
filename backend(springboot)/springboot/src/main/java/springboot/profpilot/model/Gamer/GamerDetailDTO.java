5609da5adfc5a8e375b5b3d21bfb461efcf31d2c
zkskl322 <dbawo530@gmail.com>
2024-06-18 17:33:39 +0900

    game detail

--- backend(springboot)/springboot/src/main/java/springboot/profpilot/model/Gamer/GamerDetailDTO.java
+++ backend(springboot)/springboot/src/main/java/springboot/profpilot/model/Gamer/GamerDetailDTO.java
@@ -10,26 +10,11 @@ public class GamerDetailDTO {
     private Long winScore;
     private Long loseScore;
     private Long drawScore;
-    private Double winRate;
 
     public GamerDetailDTO(String realname, Long winScore, Long loseScore, Long drawScore) {
         this.realname = realname;
         this.winScore = winScore;
         this.loseScore = loseScore;
         this.drawScore = drawScore;
-        this.winRate = calculateWinRate(winScore, loseScore, drawScore);
-    }
-
-    private Double calculateWinRate(Long winScore, Long loseScore, Long drawScore) {
-        if(winScore == null || loseScore == null || drawScore == null) {
-            return null;
-        }
-
-        Long totalGames = winScore + loseScore + drawScore;
-        if(totalGames == 0) {
-            return 0.0;
-        } else {
-            return (double) winScore / totalGames;
-        }
     }
 }
